e5f2e720e6217c52f6656f91ac43b13e2096ad43
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-02-15 18:11:28 +0200

    Root layout structure

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import Topbar from "../components/shared/Topbar";
+import LeftSidebar from "../components/shared/LeftSidebar";
+import { Outlet } from "react-router-dom";
+import BottomBar from "../components/shared/BottomBar";
 
 const RootLayout = () => {
   return (
-    <div>RootLayout</div>
-  )
-}
+    <div className="w-full md:flex">
+      <Topbar />
+      <LeftSidebar />
 
-export default RootLayout
+      <section className="flex flex-1 h-full">
+        <Outlet />
+      </section>
+
+      <BottomBar />
+    </div>
+  );
+};
+
+export default RootLayout;
